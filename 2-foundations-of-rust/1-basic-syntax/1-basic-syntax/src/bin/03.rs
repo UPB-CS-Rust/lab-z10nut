a321058de0b18b9e1c9df85d710c49051b44c9b0
Ionut <moraruionut899@gmail.com>
2024-10-07 19:50:04 +0300

    Lab1

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -2,6 +2,16 @@ fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
     // TODO
+    let mut min = input[1];
+    let mut max = input[1];
+    for n in input { 
+     if min > input[n] {
+        min = input[n];
+     }
+     if max < input[n] {
+        max = input[n];
+     }
+    }
 
-    println!("{} is largest and {} is smallest");
+    println!("{} is largest and {} is smallest", max, min);
 }
